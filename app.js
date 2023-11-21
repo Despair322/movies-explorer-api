e75440b279a903ef21a4775e34549a6a1be0e1b1
Evgeniy Dymov <krionic1@gmail.com>
2023-11-21 17:01:40 +0300

    Добавляет мидлверы

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const { errors } = require('celebrate');
 const cors = require('cors');
 require('dotenv').config();
 
+const errorHandler = require('./middlewares/error-handler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/movies' } = process.env;
 
 const app = express();
@@ -14,3 +17,13 @@ app.use(cors());
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(requestLogger);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Movies explorer app listening on port ${PORT}`);
+});
