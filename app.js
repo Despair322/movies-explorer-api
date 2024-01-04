95b16d8f5d539a0150b7f0e56b066c870472b3b5
Evgeniy Dymov <krionic1@gmail.com>
2024-01-04 22:18:31 +0300

    Добавялет корс для отладки

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ mongoose.connect(DB_URL, {}).then(() => {
 const app = express();
 
 app.use(limiter);
-app.use(cors({ credentials: true, origin: 'movie-explorer.dymov.nomoredomainsmonster.ru' }));
+app.use(cors({
+  credentials: true,
+  origin: ['movie-explorer.dymov.nomoredomainsmonster.ru',
+    'localhost:3000'],
+}));
 app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
